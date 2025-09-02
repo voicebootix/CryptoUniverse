91620da7a0c53f6715636aaf6bd41c47bcfb639f
siva168 <sivah168@gmail.com>
2025-09-02 09:47:13 +0530

    Fix Auth  issue 2

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,7 +6,7 @@ import {
   Navigate,
 } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-// import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+// import { ReactQueryDevtools } from '@tanstack/react-query-devtools'.;
 import { Toaster } from "@/components/ui/toaster";
 import { useAuthStore, useIsAuthenticated } from "@/store/authStore"; 
 
